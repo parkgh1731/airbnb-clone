cfce7f48bc922c8a4251b9bcd975496d6aae1d1e
jpark <jpark@brycen.co.jp>
2022-09-15 17:56:09 +0900

    Toy Django Project2022/09/15

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.views import View
+import os
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 
 class LoginView(FormView):
@@ -23,4 +23,47 @@ class LoginView(FormView):
 
 def log_out(request):
     logout(request)
+    return redirect(reverse("core:home"))
+
+
+def github_login(request):
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
+
+
+def github_callback(request):
+    pass
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        # 나중에 이메일 동작 테스트 할 때 주석 풀기.
+        # user.verify_email()
+        return super().form_valid(form)
+
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # to do: add succes message
+    except models.User.DoesNotExist:
+        # to do: add error message
+        pass
     return redirect(reverse("core:home"))
